0c1ea4557df77cc426255cc455626f3a59145ae3
Ram <heath.raman@gmail.com>
2017-06-18 09:50:14 +0530

    Comment destroy and badges

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,19 @@ class CommentsController < ApplicationController
     @comment.save
   end
 
+  def destroy
+    @id = params[:id]
+    @comment = Comment.find(@id)
+    @comment.destroy
+    respond_to do |format|
+      format.js
+      format.html do
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
+      end
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:msg, :user_id, :micropost_id)
